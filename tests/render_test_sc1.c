253195dca197ab4b2d349b5df974a3b2c21ff7e8
Thomas White <taw@bitwiz.org.uk>
2013-03-09 22:18:34 +0100

    tests/render_test_sc1: Check return value from sc_unpack()

--- tests/render_test_sc1.c
+++ tests/render_test_sc1.c
@@ -78,6 +78,11 @@ int main(int argc, char *argv[])
 
 	fr = sc_unpack("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. \\f{Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros.} Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.");
 
+	if ( fr == NULL ) {
+		fprintf(stderr, "sc_unpack() failed.\n");
+		return 1;
+	}
+
 	sty = calloc(1, sizeof(struct style));
 	sty->lop.pad_l = 10.0;
 	sty->lop.pad_r = 10.0;
